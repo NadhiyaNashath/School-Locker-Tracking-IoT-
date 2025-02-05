0f2b823dcac8938540a20e9cbd2230b8ab29eadc
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-02-05 08:13:37 +0530

    changes

--- logs/index.js
+++ logs/index.js
@@ -15,15 +15,15 @@ mongoose.connect("mongodb://localhost:27017/lockerLogs", {
 const db = mongoose.connection;
 
 db.on("connected", () => {
-    console.log("✅ MongoDB connected successfully!");
+    console.log("MongoDB connected successfully!");
 });
 
 db.on("error", (err) => {
-    console.error("❌ MongoDB connection error:", err);
+    console.error("MongoDB connection error:", err);
 });
 
 db.on("disconnected", () => {
-    console.log("⚠️ MongoDB disconnected.");
+    console.log("MongoDB disconnected.");
 });
 
 
